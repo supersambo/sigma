a6c3538f1869797145d8423eb03e336320f67efe
supersambo <schloegl.stephan@gmail.com>
2015-07-25 14:12:38 +0200

    changed shiny example in order to reflect backward reportig function in shiny apps

--- inst/examples/ediaspora-shiny.R
+++ inst/examples/ediaspora-shiny.R
@@ -4,12 +4,29 @@ library(sigma)
 gexf <- system.file("examples/ediaspora.gexf.xml", package = "sigma")
 
 ui = shinyUI(fluidPage(
-  checkboxInput("drawEdges", "Draw Edges", value = TRUE),
-  checkboxInput("drawNodes", "Draw Nodes", value = TRUE),
-  sigmaOutput('sigma')
+  sidebarPanel(
+      checkboxInput("drawEdges", "Draw Edges", value = TRUE),
+      checkboxInput("drawNodes", "Draw Nodes", value = TRUE),
+
+      textOutput("clickNode"),
+      textOutput("overNode")
+  ),
+  mainPanel(
+      sigmaOutput('sigma')
+  )
 ))
 
 server = function(input, output) {
+
+    output$clickNode <- renderText({ 
+         paste("clickNode:",input$clickNode)
+    })
+
+    output$overNode <- renderText({ 
+         paste("overNode:",input$overNode)
+    })
+
+
   output$sigma <- renderSigma(
     sigma(gexf, 
           drawEdges = input$drawEdges, 
